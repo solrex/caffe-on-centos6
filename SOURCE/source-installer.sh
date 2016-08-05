2d3ec930046252eb1fb5c7589af54ca6476eb7e0
Wenbo Yang <solrex@gmail.com>
2016-08-05 11:59:34 +0800

    Fix build problem on AVS-disabled CPU.

--- SOURCE/source-installer.sh
+++ SOURCE/source-installer.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 if [ "$1" == "" ]; then
-    PREFIX=/opt/anaconda2
+    PREFIX=/usr/local
 else
     PREFIX=$1
 fi
-BUILD_THREAD=20
+
+CPU_CORES=`grep processor /proc/cpuinfo | wc -l`
+HAS_AVX=`grep flags /proc/cpuinfo | grep avx | wc -l`
+HAS_GPU=`lspci | grep VGA | grep NVIDIA | wc -l`
 
 # boost
 if [ ! -f boost_1_61_0.tar.bz2 ]; then
@@ -14,7 +17,7 @@ fi
 tar -xjf boost_1_61_0.tar.bz2 || exit 1
 cd ./boost_1_61_0/
 ./bootstrap.sh --prefix=$PREFIX || exit 1
-./b2 install -j $BUILD_THREAD || exit 1
+./b2 install -j $CPU_CORES || exit 1
 cd ..
 
 # opencv
@@ -23,6 +26,13 @@ if [ ! -f opencv-2.4.13.zip ]; then
 fi
 unzip opencv-2.4.13.zip || exit 1
 cd ./opencv-2.4.13/
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DENABLE_AVX=ON -DCUDA_ARCH_BIN="3.5" || exit 1
-make install -j $BUILD_THREAD || exit 1
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
+if [ $HAS_AVX -ne 0 ]; then
+    CMAKE_FLAGS="$CMAKE_FLAGS -DENABLE_AVX=ON"
+fi
+if [ $HAS_GPU -ne 0 ]; then
+    CMAKE_FLAGS="$CMAKE_FLAGS -DCUDA_ARCH_BIN=3.5"
+fi
+cmake $CMAKE_FLAGS || exit 1
+make install -j $CPU_CORES || exit 1
 cd ..
